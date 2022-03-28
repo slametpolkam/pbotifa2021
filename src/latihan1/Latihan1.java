9d637f7a797853f933f041224e22470692125494
slametpolkam <slamet@poltek-kampar.ac.id>
2022-03-28 11:53:09 +0700

    Latihan 1 PBO Kelas 1A 2021

---
+++ src/latihan1/Latihan1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author Slamet Triyanto
+ * 21/03/2022
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        System.out.println("Halo Dunia");
+        for(int i=0;i<10;i++){
+            for(int z=0;z<=i;z++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
